66fbf1a2aadac31850b1312be05c437867a6f1a1
jukai.zhang <jukai.zhang@rea-group.com>
2016-11-21 09:30:48 +0800

    implement main page

--- card.js
+++ card.js
@@ -8,68 +8,48 @@ var cards = [
     {name: "6", desc: "this is 6"},
     {name: "7", desc: "this is 7"}
 ];
-var timeFlag = -1;
-var renderCards = function renderCards(cards) {
-    timeFlag++;
-    var outputArray = [];
-    var elements = document.getElementsByClassName("card");
-    for (var index = 0; index < elements.length; index++) {
-        outputArray[index] = (index % cards.length + elements.length * timeFlag) % cards.length;
+
+(function (Trello) {
+    var timeFlag = -1;
+    var renderCards = function renderCards(cards) {
+        timeFlag++;
+        var outputArray = [];
+        var elements = document.getElementsByClassName("card");
+        for (var index = 0; index < elements.length; index++) {
+            outputArray[index] = (index % cards.length + elements.length * timeFlag) % cards.length;
+        }
+        for (var index = 0; index < outputArray.length; index++) {
+            elements[index].childNodes[0].nodeValue = cards[outputArray[index]].name;
+            elements[index]["cardIndex"] = outputArray[index];
+        }
+    };
+
+    function authenticationSuccess() {
+        console.log('Successful authentication');
     }
-    for (var index = 0; index < outputArray.length; index++) {
-        elements[index].childNodes[0].nodeValue = cards[outputArray[index]].name;
-        elements[index]["cardIndex"] = outputArray[index];
+
+    function authenticationFailure() {
+        console.log('Failed authentication');
     }
-};
 
-function start() {
-    renderCards(cards);
-    setInterval(renderCards, 10000, cards);
-}
+    Trello.authorize({
+        type: 'popup',
+        name: 'Getting Started Application',
+        scope: {
+            read: 'true',
+            write: 'true'
+        },
+        expiration: 'never',
+        success: authenticationSuccess,
+        error: authenticationFailure
+    });
 
-start();
+    $.get("https://api.trello.com/1/boards/57f458c071beece565f9772d/cards?lists=open&list_fields=name&fields=name,desc&key=186a85bd4aa2a21068b3ebb9fafd69f4&token=729bffdc82324cfb1fe7fa0646b02806d1030df485bf2871fc6edb50fbefe92f", function (res) {
+        cards = res;
+        renderCards(cards);
+        setInterval(renderCards, 8000, cards);
+    })
 
-//(function (Trello) {
-//    var timeFlag = -1;
-//    var renderCards = function renderCards(cards) {
-//        timeFlag++;
-//        var outputArray = [];
-//        var elements = document.getElementsByClassName("card");
-//        for (var index = 0; index < elements.length; index++) {
-//            outputArray[index] = (index % cards.length + elements.length * timeFlag) % cards.length;
-//        }
-//        for (var index = 0; index < outputArray.length; index++) {
-//            elements[index].childNodes[0].nodeValue = cards[outputArray[index]].name;
-//            elements[index]["cardIndex"] = outputArray[index];
-//        }
-//    };
-//
-//    function authenticationSuccess() {
-//        console.log('Successful authentication');
-//    }
-//
-//    function authenticationFailure() {
-//        console.log('Failed authentication');
-//    }
-//
-//    Trello.authorize({
-//        type: 'popup',
-//        name: 'Getting Started Application',
-//        scope: {
-//            read: 'true',
-//            write: 'true'
-//        },
-//        expiration: 'never',
-//        success: authenticationSuccess,
-//        error: authenticationFailure
-//    });
-//
-//    $.get("https://api.trello.com/1/boards/57f458c071beece565f9772d/cards?lists=open&list_fields=name&fields=name,desc&key=186a85bd4aa2a21068b3ebb9fafd69f4&token=729bffdc82324cfb1fe7fa0646b02806d1030df485bf2871fc6edb50fbefe92f", function (res) {
-//        cards = res;
-//        renderCards(cards);
-//        setInterval(renderCards, 5000, cards);
-//    })
-//
-//})(Trello);
+})(Trello);
 
 
